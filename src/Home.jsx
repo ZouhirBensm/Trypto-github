62e387ff435a5d45ff5daefb5dd14d483ede68fd
Zouhir Bensmaia <freelancebenz@gmail.com>
2022-07-23 17:31:11 -0400

    commented example for lazy load component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,24 +1,22 @@
 
-import loadable from "@loadable/component";
-import Loading from "./Loading";
-// import ReactDOM from 'react-dom'
-// import React from 'react'
+// import loadable from "@loadable/component";
+// import Loading from "./Loading";
 
 
-const LoadableComponent = loadable(() => import("./App"), {
-  fallback: <Loading />
-});
+// const LoadableComponent = loadable(() => import("./App"), {
+//   fallback: <Loading />
+// });
 
 
-class Home extends React.Component {
-  render() {
-    return <LoadableComponent />;
-  }
-}
-const element = <Home />;
+// class Home extends React.Component {
+//   render() {
+//     return <LoadableComponent />;
+//   }
+// }
+// const element = <Home />;
 
-ReactDOM.render(element, document.getElementById('react-div'));
+// ReactDOM.render(element, document.getElementById('react-div'));
 
-export default Home
+// export default Home
 
 // https://v5.reactrouter.com/web/guides/code-splitting
