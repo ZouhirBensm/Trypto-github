9e64fdcd25158f5d2533f8c59c1e2d26cb13585b
Zouhir Bensmaia <freelancebenz@gmail.com>
2022-08-23 15:19:04 -0400

    changed the ipn url for handshake

--- config/stag.js
+++ config/stag.js
@@ -9,5 +9,5 @@ module.exports = {
   paypal_product_id: process.env.PAYPAL_PRODUCT_ID, // Product linked to sb-mzq9r20359069@business.example.com, throught to the BidBlock Development Staging created app
   paypal_client_id: process.env.PAYPAL_CLIENT_ID, // BidBlock Development Staging created app client ID linked to sb-mzq9r20359069@business.example.com
   paypal_secret: process.env.PAYPAL_SECRET, // BidBlock Development Staging created app secret linked to sb-mzq9r20359069@business.example.com
-  paypal_ipn_verification_url: 'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr' // When IPN, the handshake system deals with this URL, and the IPN URL is set up to the domain i.e. https://hidden-plateau-87550.herokuapp.com/paypal/ipn for example
+  paypal_ipn_verification_url: 'https://ipnpb.paypal.com/cgi-bin/webscr' // When IPN, the handshake system deals with this URL, and the IPN URL is set up to the domain i.e. https://hidden-plateau-87550.herokuapp.com/paypal/ipn for example
 }
