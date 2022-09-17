461515960aa340e2f99751704060df82766ef456
Zouhir Bensmaia <freelancebenz@gmail.com>
2022-09-17 11:59:29 -0400

    fixing staging deployment

--- config/prod.js
+++ config/prod.js
@@ -10,9 +10,6 @@ module.exports = {
   paypal_client_id: process.env.PAYPAL_CLIENT_ID, // BidBlock Development Staging created app client ID linked to sb-mzq9r20359069@business.example.com
   paypal_secret: process.env.PAYPAL_SECRET, // BidBlock Development Staging created app secret linked to sb-mzq9r20359069@business.example.com
   paypal_api_root: process.env.PAYPAL_API_ROOT,
-}
-
-
 // TODO set up prod enrivonments on the server
 // DATABASE_LINK: mongodb+srv://Maestro:DB%24%251993@cluster0.81z5d.mongodb.net/mern_database_atlas
 // EXPRESS_SECRET: keyboard cat
@@ -24,3 +21,4 @@ module.exports = {
 // PAYPAL_CLIENT_ID: ATXJhXxcZNV30C3S1vll7GE8VfNhZLnRfkz1dfS7ic1PTRQI8k7e8FwQWIOwoFBJs6nMM49JIVnESF_f
 // PAYPAL_SECRET: EMCZmiBejeSf5Hkik7HrtjZewPE3-5JCrjwkzWVZOEUw1jvzfg-3gBS6XKeg_vVhuIA00OEuLBvId8of
 // PAYPAL_API_ROOT: https://api-m.sandbox.paypal.com/v1
+}
