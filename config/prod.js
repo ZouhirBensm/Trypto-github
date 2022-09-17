cbced2fd4389b531ee7a806e751613fe7fdd8a69
Zouhir Bensmaia <freelancebenz@gmail.com>
2022-09-17 12:04:03 -0400

    fixing staging deployment

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  database_link: process.env.DATABASE_LINK,
+  database_link: process?.env.DATABASE_LINK,
   express_session_secret: process.env.EXPRESS_SECRET,
   port: process.env.PORT,
   domain: process.env.REACT_APP_DOMAIN,
