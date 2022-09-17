e56c70f3933991b9f721b7de09583633b50b042e
Zouhir Bensmaia <freelancebenz@gmail.com>
2022-09-17 11:49:23 -0400

    fixing staging deployment

--- config/base.js
+++ config/base.js
@@ -1,11 +1,14 @@
 switch (process.env.NODE_ENV) {
   case 'staging':
+    console.log("Exporting Staging variables")
     module.exports = require('./stag')
     break;
   case 'production':
+    console.log("Exporting Production variables")
     module.exports = require('./prod')
     break;
   case 'development':
+    console.log("Exporting Development variables")
     module.exports = require('./dev')
     break;
   default:
