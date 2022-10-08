0cd499802b7474669dca9d0b85961382f76a9cf6
Zouhir Bensmaia <freelancebenz@gmail.com>
2022-10-08 16:23:14 -0400

    Added the description field with update capacity

--- server/home-currencyorders-registerlogin-backend.js
+++ server/home-currencyorders-registerlogin-backend.js
@@ -246,7 +246,7 @@ homeOrdersBackend_app_router.get('/logout', require_loggedin_for_data(true), (re
   })
 })
 
-// TODO ! add description for market orders
+
 // TODO add userID for articles
 homeOrdersBackend_app_router.delete('/users/profile/delete/:userId', require_loggedin_for_data(true), authenticate_role_for_data([ROLE.MASTER, ROLE.USER.NOTSUBSCRIBER, ROLE.USER.SUBSCRIBER.BASIC]), requester_auth_middleware(4), startEmptyNotificationsMiddleware, deleteBuyCryptoOrdersMiddleware, deleteSellOrdersMiddleware, deleteMarketOrderMiddleware, deleteProtagonistsMiddleware, deleteMessagesMiddleware,
 sessionSubscriberMiddleware, deleteEffectUserToUnsubscribeMiddleware, deleteUserMiddleware, logoutMiddleware, (req,res,next)=>{
