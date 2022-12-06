42c2aad0f8b27cf02e228ec6364e0ad7457bf24c
Zouhir Bensmaia <freelancebenz@gmail.com>
2022-12-06 13:06:02 -0500

    trusted proxies 1

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const Agenda = require('agenda');
 const rateLimit = require('express-rate-limit')
 
 const limiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minutes
+  // windowMs: 60 * 1000, // 1 minutes
   // windowMs: 5 * 60 * 1000, // 5 minutes
-	// windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 30, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	// max: 200 * 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	windowMs: 15 * 60 * 1000, // 15 minutes
+  // max: 30, // Limit each IP to 30 requests per `window` (here, per 15 minutes)
+	max: 200 * 5, // Limit each IP to 1000 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
