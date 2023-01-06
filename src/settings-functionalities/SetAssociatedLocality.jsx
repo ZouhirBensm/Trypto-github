dd230959a287622524123f8e56e295a970e6c6ea
Zouhir Bensmaia <freelancebenz@gmail.com>
2023-01-06 14:42:34 -0500

    Added comments of what needs to be done

--- src/settings-functionalities/SetAssociatedLocality.jsx
+++ src/settings-functionalities/SetAssociatedLocality.jsx
@@ -1,5 +1,24 @@
 
 class SetAssociatedLocality extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+
+  // Retrieve the user id
+  // Retrive the user location
+  // If user has a associated locality
+  // have the state update to that user locality
+  // render a google maps centered in the center of that locality, with a pin on it
+
+  // If user does not have a associated locality
+  // Keep the state locality state undefined
+  // Have the map and pin centered in Ottawa
+
+  // When user edits the locality, through the pin or autocomplete
+  // Update the master components location state
+  // pass the state to the child Submit component and on every submit edit tge associated locality on the backend
+  
   render(){
     return (
       <div>SetAssociatedLocality...</div>
