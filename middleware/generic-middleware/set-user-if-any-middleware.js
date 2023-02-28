3a0d821011e5e07cf94b9a68f7032792e38bd83f
Zouhir Bensmaia <freelancebenz@gmail.com>
2023-02-28 16:49:08 -0500

    Trying to fix staging bug

--- middleware/generic-middleware/set-user-if-any-middleware.js
+++ middleware/generic-middleware/set-user-if-any-middleware.js
@@ -8,6 +8,7 @@ async function set_user_if_any(req, res, next) {
     // console.log("\n\nUSER:", userfindOne)
     res.locals.user = userfindOne
   } catch (e) {
+    const error = new Error("No user session was found")
     return next(error)
   }
   return next()
