1e2e7b807e5274a78d2b3f9e42359df68b4d48c5
Zouhir Bensmaia <freelancebenz@gmail.com>
2023-06-17 13:54:21 -0400

    Added the TODOs on the app

--- models/articles-models/ArticleHeadTag.js
+++ models/articles-models/ArticleHeadTag.js
@@ -44,3 +44,19 @@ const ArticleHeadTagSchema = new Schema({
 
 const ArticleHeadTag = mongoose.model('ArticleHeadTag', ArticleHeadTagSchema)
 module.exports = ArticleHeadTag
+
+
+// TODO !!!
+// first 2 things I noticed. 
+
+// 1- you don't have protection against spam emails-> home page, need to protect against fake emails
+
+// 2- the cursor doesn't turn to pointer on hover -> check if true in all buttons and inputs?
+
+// 3- The login form is not even a form, I can't click enter to login, I have to click by mouse -> Add enter key press on forms
+
+// 4- needs more css editing on phone, it looks bad on phone -> Fix the home page CSS to look exactly like the designs
+
+
+// TODO !!! Simplify the copy right on the home banner: drop the fluff. dont welcome get straight to the point to keep things simple.
+// "Bitcoin only marketplace" be better?
