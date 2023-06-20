f13324cd336a1bf430f94a7d9f7ea468ae02499e
Zouhir Bensmaia <freelancebenz@gmail.com>
2023-06-20 07:44:01 -0400

    About to add the capacity to add a synthesis

--- controllers/article-controllers/individual-article-controller.js
+++ controllers/article-controllers/individual-article-controller.js
@@ -1,6 +1,6 @@
 function controller1(req, res, next) {
   // res.locals.article_title = req.params.article_title ?  req.params.article_title : undefined
-  res.locals.header = 2
+  res.locals.head = 2
 
 
   res.render('bodies/bidblock-blog-article')
