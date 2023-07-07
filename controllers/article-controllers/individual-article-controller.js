205d5f09ea6de0fc166e7210eb775808f0ee30f2
Zouhir Bensmaia <freelancebenz@gmail.com>
2023-07-07 11:41:27 -0400

    Done

--- controllers/article-controllers/individual-article-controller.js
+++ controllers/article-controllers/individual-article-controller.js
@@ -4,50 +4,48 @@ async function controller1(req, res, next) {
   // res.locals.article_title = req.params.article_title ?  req.params.article_title : undefined
   res.locals.head = 2
 
-  
+  let EMAIL_blocks = await res.locals.article.articlenesteddata_id.blocks.filter(block => { return block.type == SECTION_TYPES.EMAIL });
 
 
-    let EMAIL_blocks = await res.locals.article.articlenesteddata_id.blocks.filter(block => {return block.type == SECTION_TYPES.EMAIL});
+  if (!EMAIL_blocks) return next()
+
+  res.locals.EMAIL_blocks = EMAIL_blocks
 
 
-    if(!EMAIL_blocks) return next()
+  for (let index = 0; index < EMAIL_blocks.length; index++) {
+    let block = EMAIL_blocks[index];
 
-    res.locals.EMAIL_blocks = EMAIL_blocks
+    // TODO !!!! look at diff between new3, new2 and new
+    // TODO !!!!! HERE figure out why requires a copy object?
 
+    const copiedObj = JSON.parse(JSON.stringify(block));
 
-    for (let index = 0; index < EMAIL_blocks.length; index++) {
-      let block = EMAIL_blocks[index];
+    const titleKey = `title${index}`;
+    const subtitleKey = `subtitle${index}`;
 
-      // TODO !!!! look at diff between new3, new2 and new
-      // TODO !!!!! HERE figure out why requires a copy object?
-
-      const copiedObj = JSON.parse(JSON.stringify(block));
-
-      const titleKey = `title${index}`;
-      const subtitleKey = `subtitle${index}`;
-
-      res.locals[titleKey] = copiedObj.EMAIL_title;
-      res.locals[subtitleKey] = copiedObj.EMAIL_subtitle;
-    }
+    res.locals[titleKey] = copiedObj.EMAIL_title;
+    res.locals[subtitleKey] = copiedObj.EMAIL_subtitle;
+  }
 
 
 
-    return next()
+  return next()
 
 }
 
 
 
-async function controller2(req, res, next) { 
+async function controller2(req, res, next) {
 
-    var JSX_to_load = 'OnPageFooter';
+  var JSX_to_load = 'OnPageFooter';
 
-    var JSX_to_load2 = 'EmailMarketingCollector';
+  var JSX_to_load2 = 'EmailMarketingCollector';
 
-    res.render('bodies/bidblock-blog-article', { 
-      JSX_to_load : JSX_to_load, 
-      JSX_to_load2 : JSX_to_load2, 
-    })
+
+  res.render('bodies/bidblock-blog-article', {
+    JSX_to_load: JSX_to_load,
+    ...(res.locals.EMAIL_blocks && { JSX_to_load2: JSX_to_load2 }),
+  })
 
 }
 
