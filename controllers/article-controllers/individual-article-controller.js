5aadceac01af8cf5502384f922ed80930e290de7
Zouhir Bensmaia <freelancebenz@gmail.com>
2023-07-07 21:31:42 -0400

    Need to test embed in order to stop errors

--- controllers/article-controllers/individual-article-controller.js
+++ controllers/article-controllers/individual-article-controller.js
@@ -21,22 +21,24 @@ async function controller2(req, res, next) {
 }
 
 
-// TODO !!!! Test schemas and renders
 
 
-// TODO !!!! add table of content blocks. dialog
+// TODO !!!! Youtube embed source of errors
+// TODO !!! Need to add itemprops for EMBED component and for EMAIL component.
+// TODO !!!! resave the back up
+// TODO !!!! When editing a content structure reset the nested block
 
 
-// a bunch of google errors on the article page on staging, test without the embed
-// the send button on the article page (email collector) has a higher z index than the sidenav
-// resave the back up
+// TODO !!!! add table of content blocks in dialog
+// TODO !!! Need to add itemprops for  table of content.
+
+
 
 // TODO !!!!! sitemap build
 // TODO !!!!! CRUDE Capabilities for blog articles
 // TODO !!!!! build HTML sitemap
 // TODO !!!!! google search console register
 // TODO !!!!! google analitics register
-// TODO !!! Need to add itemprops for EMBED component and for EMAIL component, and table of content also
 // TODO !!! Look into surfer ai
 // TODO !!! Build chatGPT generator on aiaudiotoblog.bidblock.ca on another localhost:3001? and deploy scripts
 // TODO !!!! Need to optimise Blog Loads,
